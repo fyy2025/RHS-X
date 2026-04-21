8100faf5c52a9784d137959af66e6681381905b9
Soumyakanti Pan <soumyakanti8051@gmail.com>
2026-04-20 20:13:22 -0700

    submit scenario2 with mail support

--- Code/run.sh
+++ Code/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 1. Submit simulation
-ID=$(sbatch --parsable trial.slurm)
+ID=$(sbatch --parsable submit.slurm)
 echo "Job $ID submitted."
 
 # 2. Run a "silent" background process on the login node
@@ -14,7 +14,7 @@ nohup bash -c "
 
     # Now that it's done, we are STILL on the login node.
     # Send the email.
-    mail -s 'Results $ID' -a results_${ID}.zip span18@uw.edu
+    mail -s 'Results $ID' -a results_${ID}.zip span18@uw.edu fyy2030@uw.edu
 " > log/email_watcher.log 2>&1 &
 
 echo "Watcher started. You can log out. The login node will email you later."
